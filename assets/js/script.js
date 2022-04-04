e4b102291e362390e979557e736fec719be17d92
Casres <christian@casillas.tech>
2022-04-04 03:32:00 -0400

    SP/ finished with previous problem, added more styling and logic in regards to error capture

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,3 @@
-
-
-
-
 var temp = $("#temperature");
 var wind = $("#wnd");
 var humid = $("#humidity-reading");
@@ -41,86 +37,91 @@ var displaySearchHistory = function () {
 };
 
 function weatherData(userInputData) {
-    
-    var cityTitle = $("#city-name-title"); 
-    $(cityTitle).addClass("city-title-style");
-    cityTitle.text(userInputData);
+  var cityTitle = $("#city-name-title");
+  $(cityTitle).addClass("city-title-style");
+  cityTitle.text(userInputData);
 
+  // current day weather API
+  var apiUrl1 =
+    "https://api.openweathermap.org/data/2.5/weather?q=" +
+    userInputData +
+    "&appid=b11cb0cfc1337df893547ad4b4c74492&units=imperial";
+  fetch(apiUrl1).then(function (data) {
+    if (data.status === 404) {
+      alert("City not found");
+      return;
+    } else {
+      data.json().then(function (info) {
+      var latCord = info.coord.lat;
+      var lonCord = info.coord.lon;
+      console.log();
 
+      var apiUrl2 =
+        "https://api.openweathermap.org/data/2.5/onecall?lat=" +
+        latCord +
+        "&lon=" +
+        lonCord +
+        "&exclude=hourly,minutely,alerts&appid=b11cb0cfc1337df893547ad4b4c74492&units=imperial";
+      fetch(apiUrl2).then(function (data) {
+        data.json().then(function (info) {
+          // current day date
+          // var currentDayDate = info.daily[i].dt;
 
-    // current day weather API
-    var apiUrl1 =
-        "https://api.openweathermap.org/data/2.5/weather?q=" + userInputData + "&appid=b11cb0cfc1337df893547ad4b4c74492&units=imperial";
-    fetch(apiUrl1).then(function (data) {
-    data.json().then(function (info) {
-        var latCord = info.coord.lat;
-        var lonCord = info.coord.lon;
-        console.log();
+          // current day temperature
+          var currentTemp = info.current.temp;
+          temp.text(currentTemp);
+          // current day wind speed
+          var currentWind = info.current.wind_speed;
+          wind.text(currentWind);
+          // current day humidity
+          var currentHumidity = info.current.humidity;
+          humid.text(currentHumidity);
+          // current day UV
+          var currentUV = info.current.uvi;
+          uvi.text(currentUV);
 
-        var apiUrl2 =
-            "https://api.openweathermap.org/data/2.5/onecall?lat=" + latCord + "&lon=" + lonCord + "&exclude=hourly,minutely,alerts&appid=b11cb0cfc1337df893547ad4b4c74492&units=imperial";
-        fetch(apiUrl2).then(function (data) {
-            data.json().then(function (info) {
-                // current day date
-                // var currentDayDate = info.daily[i].dt;
+          // 5 day forecast
+          var days = $(".weekday-report");
+          for (let i = 0; i < days.length; i++) {
+            var day = days[i];
 
-                // current day temperature
-                var currentTemp = info.current.temp;
-                temp.text(currentTemp);
-                // current day wind speed
-                var currentWind = info.current.wind_speed;
-                wind.text(currentWind);
-                // current day humidity
-                var currentHumidity = info.current.humidity;
-                humid.text(currentHumidity);
-                // current day UV
-                var currentUV = info.current.uvi;
-                uvi.text(currentUV);
+            var dateInfo = new Date(info.daily[i].dt * 1000);
+            // var dateLayout = dateInfo.getDate(DD-MM-YYYY);
+            var dateDay = day.children[0];
+            dateDay.innerText = dateInfo;
 
-                // 5 day forecast
-                var days = $(".weekday-report");
-                for (let i = 0; i < days.length; i++) {
-                var day = days[i];
+            // grabs the needed info rom API
+            var tempReport = info.daily[i].temp.day;
+            // points to parent element that holds the child element where the info goes
+            var tempDay = day.children[2];
+            // points to the child element (which in this case is a <span>) that the info goes into
+            var tempDay2 = tempDay.children[0];
+            // puts the info, grabbed from the API into the span element
+            tempDay2.innerText = tempReport;
 
-                var dateInfo = new Date(info.daily[i].dt*1000);
-                // var dateLayout = dateInfo.getDate(DD-MM-YYYY);
-                var dateDay = day.children[0];
-                dateDay.innerText = dateInfo;
+            // -----------------------------//
 
-                // grabs the needed info rom API
-                var tempReport = info.daily[i].temp.day;
-                // points to parent element that holds the child element where the info goes
-                var tempDay = day.children[2];
-                // points to the child element (which in this case is a <span>) that the info goes into
-                var tempDay2 = tempDay.children[0];
-                // puts the info, grabbed from the API into the span element
-                tempDay2.innerText = tempReport;
+            // does the same for the wind spd
+            var windSpdReport = info.daily[i].wind_speed;
+            var windSpdDay = day.children[3];
+            var windSpdDay2 = windSpdDay.children[0];
+            windSpdDay2.innerText = windSpdReport;
 
-                // -----------------------------//
+            // -----------------------------//
 
-                // does the same for the wind spd
-                var windSpdReport = info.daily[i].wind_speed;
-                var windSpdDay = day.children[3];
-                var windSpdDay2 = windSpdDay.children[0];
-                windSpdDay2.innerText = windSpdReport;
-
-                // -----------------------------//
-
-                // does the same for humidity
-                var humidityReport = info.daily[i].humidity;
-                var humidityDay = day.children[4];
-                var humidityDay2 = humidityDay.children[0];
-                humidityDay2.innerText = humidityReport;
-                }
-
-            });
+            // does the same for humidity
+            var humidityReport = info.daily[i].humidity;
+            var humidityDay = day.children[4];
+            var humidityDay2 = humidityDay.children[0];
+            humidityDay2.innerText = humidityReport;
+          }
         });
+      });
     });
-    });
-};
-
+    }
+  });
+}
 
 // weather forecast, 5 days
-
 
 displaySearchHistory();
